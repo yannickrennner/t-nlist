376698ca6381f4577c73d3215c609e4cf9285dfc
yannickrennner <yannick.renner@protonmail.ch>
2017-06-19 12:45:02 +0200

    init

--- Tonlist.API/Controllers/ValuesController.cs
+++ Tonlist.API/Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace Tonlist.API.Controllers
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return "value" + id;
         }
 
         // POST api/values
